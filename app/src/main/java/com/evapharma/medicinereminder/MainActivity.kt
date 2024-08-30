a63f3240588a3390d4c7563e99d536518e815784
Ahmed Saad <ahmed.moh.saad.hussein.ali@gmail.com>
2024-08-30 10:56:29 +0300

    add popUpTo yo splashFragment_to_MedicineDetailsFragment action

--- app/src/main/java/com/evapharma/medicinereminder/MainActivity.kt
+++ app/src/main/java/com/evapharma/medicinereminder/MainActivity.kt
@@ -4,7 +4,6 @@ import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.LinearLayout
 import android.widget.ProgressBar
